87acc09323c3319a5c5d1c04af201153b3735d6a
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-03-02 02:48:14 +0500

    update

--- app/api/verify/[id]/route.ts
+++ app/api/verify/[id]/route.ts
@@ -20,14 +20,64 @@ export const GET = async (
         }
       );
     }
-    return NextResponse.json(
-      {
-        success: true,
-        message: "User updated successfully",
-      },
-      {
-        status: 200,
-      }
+    return new NextResponse(
+      ` <html lang="en">
+      <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Email Verification</title>
+      <style>
+        body {
+          font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+          background-color: #f8f9fa;
+          margin: 0;
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          align-items: center;
+          height: 100vh;
+        }
+        .container {
+          text-align: center;
+          background-color: #ffffff;
+          border-radius: 10px;
+          padding: 40px;
+          box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+          max-width: 400px;
+          width: 90%;
+        }
+        h1 {
+          color: black;
+          margin-bottom: 20px;
+        }
+        p {
+          color: #555555;
+          margin-bottom: 30px;
+        }
+        a {
+          color: #ffffff;
+          text-decoration: none;
+          background-color: #F4AE39;
+          padding: 10px 20px;
+          border-radius: 5px;
+          transition: background-color 0.3s ease;
+        }
+        a:hover {
+          background-color: #0056b3;
+        }
+      </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Your email has been verified</h1>
+          <p>You can now login.</p>
+          <a href="${process.env.NEXTAUTH_URL}/login">Go to Login Page</a>
+        </div>
+      </body>
+      </html>
+      
+  `,
+      { status: 200, headers: { "content-type": "text/html" } }
     );
   } catch (error) {
     return NextResponse.json(
