ed454c7d31312534a62a41a79b2d02944c578b40
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-07-01 03:23:53 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
-import { useEffect, useState } from "react";
-import { Welcome } from "@/components/welcome";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -14,44 +12,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [seconds, setSeconds] = useState(0);
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setSeconds((prevSeconds) => prevSeconds - 1);
-  //   }, 1000);
-
-  //   if (seconds === 0) {
-  //     clearInterval(interval);
-  //   }
-
-  //   return () => clearInterval(interval);
-  // }, [seconds]);
-
   return (
     <html lang="en" className="scrollbar scrollbar-none sm:scrollbar-thin">
       <head>
-        <title>StyleSync</title>
+        <title>Arfa & Maryam's</title>
         <link rel="icon" href="/logo.png" />
       </head>
 
       <body className={inter.className}>
-        {seconds > 0 ? (
-          <Welcome seconds={seconds} />
-        ) : (
-          <SessionProvider>
-            <Navbar />
-            <div>{children}</div>
-            <Footer />
-            <Toaster
-              toastOptions={{
-                style: {
-                  bottom: "1rem",
-                  right: "1rem",
-                },
-              }}
-            />
-          </SessionProvider>
-        )}
+        <SessionProvider>
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
+          <Toaster
+            toastOptions={{
+              style: {
+                bottom: "1rem",
+                right: "1rem",
+              },
+            }}
+          />
+        </SessionProvider>
       </body>
     </html>
   );
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { navLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
