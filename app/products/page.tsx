df9b197addcb3996f27f3c57f2d12ce31688de37
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-07-01 04:14:07 +0500

    update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,18 +7,12 @@ import axios from "axios";
 import { BiSearchAlt } from "react-icons/bi";
 import ReactLoading from "react-loading";
 import { CiFilter } from "react-icons/ci";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const page = () => {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     getProducts();
   }, []);
@@ -31,6 +25,19 @@ const page = () => {
       console.log(error);
     } finally {
       setLoading(false);
+    }
+  };
+
+  const handleSearch = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`/api/search/${search}`);
+      setProducts(response.data.products);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+      setSearch("");
     }
   };
 
@@ -79,15 +86,21 @@ const page = () => {
         <div className="md:border-l md:pl-5 w-full">
           <div className="flex gap-3 sm:gap-10 w-full items-end">
             <h2 className="font-semibold shrink-0 border p-2 text-xs sm:text-sm">
-              {products.length} Products
+              {products.length} {products.length > 1 ? "Products" : "Product"}
             </h2>
             <div className="flex items-end w-full gap-2">
               <input
                 type="text"
                 className="text-sm sm:text-base w-full border-b border-gray-300 focus:outline-none focus:border-gray-500 p-2 fond-bold"
                 placeholder="Search..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleSearch()}
               />
-              <button className="flex justify-center border py-1 border-black bg-transparent text-black  dark:border-white relative group transition duration-200">
+              <button
+                onClick={handleSearch}
+                className="flex justify-center border py-1 border-black bg-transparent text-black  dark:border-white relative group transition duration-200"
+              >
                 <div className="absolute bottom-0 right-0 bg-primary h-full w-full group-hover:scale-x-90 group-hover:scale-y-75 transition-all duration-200" />
                 <span className="flex items-center gap-1 relative text-sm font-semibold py-1 px-5">
                   <BiSearchAlt className="sm:text-lg" />{" "}
@@ -149,22 +162,24 @@ const page = () => {
               <ReactLoading type="bars" color="#E17489" />
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-5 mb-10">
-              {products.map((product: any) => (
-                <Card
-                  key={product._id}
-                  id={product._id}
-                  img={product.images[0]}
-                  img2={product.images[1]}
-                  img3={product.images[2]}
-                  img4={product.images[3]}
-                  name={product.name}
-                  description={product.description}
-                  price={product.price}
-                  sizes={product.sizes}
-                  colors={product.colors}
-                />
-              ))}
+            <div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-5 mb-10">
+                {products.map((product: any) => (
+                  <Card
+                    key={product._id}
+                    id={product._id}
+                    img={product.images[0]}
+                    img2={product.images[1]}
+                    img3={product.images[2]}
+                    img4={product.images[3]}
+                    name={product.name}
+                    description={product.description}
+                    price={product.price}
+                    sizes={product.sizes}
+                    colors={product.colors}
+                  />
+                ))}
+              </div>
             </div>
           )}
         </div>
