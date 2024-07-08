594519c4708015d28c3c86dc2edc526a995e64e0
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-07-08 12:01:26 +0500

    update

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -9,11 +9,29 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import axios from "axios";
 
 const page = () => {
   const [orders, setOrders] = useState([]);
   const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("");
+  const [paymentStatus, setPaymentStatus] = useState("");
 
   const fetchTotalOrders = async () => {
     const response = await axios.get("/api/order");
@@ -57,6 +75,7 @@ const page = () => {
             <TableHead className="text-center text-white">
               Order Status
             </TableHead>
+            <TableHead className="text-center text-white">Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -94,6 +113,162 @@ const page = () => {
                 </TableCell>
                 <TableCell className="text-center">
                   {order.status[0].toUpperCase() + order.status.slice(1)}
+                </TableCell>
+                <TableCell className="text-center">
+                  <Dialog>
+                    <DialogTrigger>
+                      <button className=" flex justify-center border py-1 border-black bg-transparent text-white dark:border-white relative group transition duration-200">
+                        <div className="absolute bottom-0 right-0 bg-primary h-full w-full group-hover:scale-x-90 group-hover:scale-y-75 transition-all duration-200" />
+                        <span className="relative text-sm font-semibold py-1 px-2">
+                          View Details
+                        </span>
+                      </button>
+                    </DialogTrigger>
+                    <DialogContent className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-lg scrollbar scrollbar-none overflow-scroll h-full md:max-h-[520px]">
+                      <DialogHeader>
+                        <DialogTitle className="text-2xl font-semibold text-gray-800">
+                          Order Details
+                        </DialogTitle>
+                      </DialogHeader>
+                      <DialogDescription>
+                        <div className="space-y-4">
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Customer Name
+                            </h1>
+                            <p className="text-gray-600">{order.name}</p>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Customer Email
+                            </h1>
+                            <p className="text-gray-600">{order.email}</p>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Customer Phone
+                            </h1>
+                            <p className="text-gray-600">{order.phone}</p>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Shipping Address
+                            </h1>
+                            <p className="text-gray-600">
+                              {order.shippingAddress.address},{" "}
+                              {order.shippingAddress.city},{" "}
+                              {order.shippingAddress.postalCode},{" "}
+                              {order.shippingAddress.country}
+                            </p>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Order Items
+                            </h1>
+                            <ul className="list-disc list-inside text-gray-600">
+                              {order.orderItems.map((item: any) => (
+                                <li key={item._id} className="ml-4">
+                                  {item.product.name} x {item.quantity} ({" "}
+                                  {item.size.toUpperCase()} |{" "}
+                                  {item.color[0].toUpperCase() +
+                                    item.color.slice(1)}{" "}
+                                  )
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Total Price
+                            </h1>
+                            <p className="text-gray-600">
+                              PKR {order.totalPrice.toLocaleString()}
+                            </p>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Payment Method
+                            </h1>
+                            <p className="text-gray-600">
+                              {order.paymentMethod.toUpperCase()}
+                            </p>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Payment Status
+                            </h1>
+                            <Select
+                              onValueChange={(value: any) => {
+                                setPaymentStatus(value);
+                              }}
+                            >
+                              <SelectTrigger className="w-fit mt-1">
+                                <SelectValue
+                                  placeholder={
+                                    <span>
+                                      {order.paymentStatus[0].toUpperCase() +
+                                        order.paymentStatus.slice(1)}{" "}
+                                    </span>
+                                  }
+                                />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="pending">Pending</SelectItem>
+                                <SelectItem value="completed">
+                                  Completed
+                                </SelectItem>
+                                <SelectItem value="canceled">
+                                  Canceled
+                                </SelectItem>
+                              </SelectContent>
+                            </Select>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Order Status
+                            </h1>
+                            <Select
+                              onValueChange={(value: any) => {
+                                setPaymentStatus(value);
+                              }}
+                            >
+                              <SelectTrigger className="w-fit mt-1">
+                                <SelectValue
+                                  placeholder={
+                                    <span>
+                                      {order.status[0].toUpperCase() +
+                                        order.status.slice(1)}{" "}
+                                    </span>
+                                  }
+                                />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="pending">Pending</SelectItem>
+                                <SelectItem value="completed">
+                                  Completed
+                                </SelectItem>
+                                <SelectItem value="canceled">
+                                  Canceled
+                                </SelectItem>
+                              </SelectContent>
+                            </Select>
+                          </div>
+                          <div>
+                            <h1 className="text-xl font-bold text-gray-700">
+                              Date of Order
+                            </h1>
+                            <p className="text-gray-600">
+                              {new Date(order.createdAt).toLocaleDateString()} -{" "}
+                              {new Date(order.createdAt).toLocaleTimeString()}
+                            </p>
+                          </div>
+                          <br />
+                          <br />
+                          <br />
+                        </div>
+                      </DialogDescription>
+                    </DialogContent>
+                  </Dialog>
                 </TableCell>
               </TableRow>
             ))}
